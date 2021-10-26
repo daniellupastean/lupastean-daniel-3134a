ce121111fdb9aa919544055fae3f73b2e4febc38
Daniel Lupastean <lupasteandaniell1@gmail.com>
2021-10-26 23:10:50 +0300

    EGC | Add triangle, manipulate color & move camera

---
+++ lupastean-daniel-3134a/Camera3D.cs
@@ -0,0 +1,117 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace lupastean_daniel_3134a
+{
+    class Camera3D
+    {
+        private Vector3 eye = new Vector3(0, 10, 30);
+        private Vector3 target = new Vector3(0, 0, 0);
+        private Vector3 up = new Vector3(0, 1, 0);
+        private const int MOVEMENT_UNIT = 1;
+
+        public void SetCamera()
+        {
+            Matrix4 camera = Matrix4.LookAt(eye, target, up);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref camera);
+
+            // Mouse.SetPosition(450, 450);
+        }
+
+        public void RotateRight()
+        {
+            if (eye.X < 40 && eye.Z>=40)
+            {
+                eye = new Vector3(eye.X + MOVEMENT_UNIT, eye.Y, eye.Z);
+            }
+            else if(eye.X>=40 && eye.Z>-40)
+            {
+                eye = new Vector3(eye.X, eye.Y, eye.Z - MOVEMENT_UNIT);
+            }
+            else if(eye.X>-40 && eye.Z<=-40)
+            {
+                eye = new Vector3(eye.X - MOVEMENT_UNIT, eye.Y, eye.Z);
+            }
+            else
+            {
+                eye = new Vector3(eye.X, eye.Y, eye.Z + MOVEMENT_UNIT);
+            }
+            SetCamera();
+        }
+
+        public void RotateLeft()
+        {
+            if (eye.X > -40 && eye.Z >= 40)
+            {
+                eye = new Vector3(eye.X - MOVEMENT_UNIT, eye.Y, eye.Z);
+            }
+            else if (eye.X <= -40 && eye.Z > -40)
+            {
+                eye = new Vector3(eye.X, eye.Y, eye.Z - MOVEMENT_UNIT);
+            }
+            else if (eye.X < 40 && eye.Z <= -40)
+            {
+                eye = new Vector3(eye.X + MOVEMENT_UNIT, eye.Y, eye.Z);
+            }
+            else
+            {
+                eye = new Vector3(eye.X, eye.Y, eye.Z + MOVEMENT_UNIT);
+            }
+            SetCamera();
+        }
+
+        public void RotateUp()
+        {
+            eye = new Vector3(eye.X, eye.Y + 5, eye.Z);
+            SetCamera();
+        }
+
+        public void RotateDown()
+        {
+            eye = new Vector3(eye.X, eye.Y - 5, eye.Z);
+            SetCamera();
+        }
+
+        public void MoveRight()
+        {
+            eye = new Vector3(eye.X, eye.Y, eye.Z - MOVEMENT_UNIT);
+            target = new Vector3(target.X, target.Y, target.Z - MOVEMENT_UNIT);
+            SetCamera();
+        }
+        public void MoveLeft()
+        {
+            eye = new Vector3(eye.X, eye.Y, eye.Z + MOVEMENT_UNIT);
+            target = new Vector3(target.X, target.Y, target.Z + MOVEMENT_UNIT);
+            SetCamera();
+        }
+
+        public void MoveForward()
+        {
+            eye = new Vector3(eye.X - MOVEMENT_UNIT, eye.Y, eye.Z);
+            target = new Vector3(target.X - MOVEMENT_UNIT, target.Y, target.Z);
+            SetCamera();
+        }
+
+        public void MoveBackward()
+        {
+            eye = new Vector3(eye.X + MOVEMENT_UNIT, eye.Y, eye.Z);
+            target = new Vector3(target.X + MOVEMENT_UNIT, target.Y, target.Z);
+            SetCamera();
+        }
+
+        public void MoveUp()
+        {
+            eye = new Vector3(eye.X, eye.Y + MOVEMENT_UNIT, eye.Z);
+            target = new Vector3(target.X, target.Y + MOVEMENT_UNIT, target.Z);
+            SetCamera();
+        }
+
+        public void MoveDown()
+        {
+            eye = new Vector3(eye.X, eye.Y - MOVEMENT_UNIT, eye.Z);
+            target = new Vector3(target.X, target.Y - MOVEMENT_UNIT, target.Z);
+            SetCamera();
+        }
+    }
+}
--- lupastean-daniel-3134a/Program.cs
+++ lupastean-daniel-3134a/Program.cs
@@ -6,15 +6,13 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 
 
 /**
     Aplicația utilizează biblioteca OpenTK v3.3
-    Se misca cubul in sus si in jos la apasarea butoanelor Up si Down si se roteste la miscarea mouse-ului cu click stanga apasat.
-    Aplicația fiind scrisă în modul consolă nu va utiliza controlul WinForms
-    oferit de OpenTK!
     Tipul de ferestră utilizat: GAMEWINDOW. Se demmonstrează modul imediat de randare (vezi comentariul!)...
 **/
 
@@ -22,205 +20,193 @@ namespace lupastean_daniel_3134a
 {
     class Program : GameWindow
     {
-        const float rotation_speed = 180.0f;
-        int translateAmount = 0;
-        int rotateAmount = 0;
-        bool showCube = true;
-        KeyboardState lastKeyPress;
+        public const int XYZ_SIZE = 75;
+        private double red = 1, green = 1, blue = 1, alpha = 1;
+        Vector3[] co = new Vector3[3];
+        private Camera3D camera;
 
-        // Constructor.
-        public Program() : base(800, 600)
+        public Program() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
         {
-            VSync = VSyncMode.On;
-        }
+            
 
-        /**Setare mediu OpenGL și încarcarea resurselor (dacă e necesar) - de exemplu culoarea de
-           fundal a ferestrei 3D.
-           Atenție! Acest cod se execută înainte de desenarea efectivă a scenei 3D. */
+            string text = System.IO.File.ReadAllText(@"./../../coordonate.txt");
+
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            string[] lines = text.Split('\n');
+
+            for (int i=0; i<lines.Length; i++)
+            {
+                string[] textCoord = lines[i].Split(' ');
+                co[i][0] = int.Parse(textCoord[0]);
+                co[i][1] = int.Parse(textCoord[1]);
+                co[i][2] = int.Parse(textCoord[2]);
+            }
+
+            VSync = VSyncMode.On;
+
+            Console.WriteLine("OpenGl versiunea: " + GL.GetString(StringName.Version));
+            Title = "OpenGl versiunea: " + GL.GetString(StringName.Version) + " (mod imediat)";
+
+            camera = new Camera3D();
+        }
+       
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
 
-            GL.ClearColor(Color.Blue);
-            //GL.Enable(EnableCap.DepthTest);
+            GL.ClearColor(Color.Black); //culoare de fundal
+            GL.Enable(EnableCap.DepthTest); //
+            GL.DepthFunc(DepthFunction.Less); //adancime
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest); // anti-aliasing
         }
 
-        /**Inițierea afișării și setarea viewport-ului grafic. Metoda este invocată la redimensionarea
-           ferestrei. Va fi invocată o dată și imediat după metoda ONLOAD()!
-           Viewport-ul va fi dimensionat conform mărimii ferestrei active (cele 2 obiecte pot avea și mărimi 
-           diferite). */
+   
         protected override void OnResize(EventArgs e)
         {
             base.OnResize(e);
-
-            GL.Viewport(0, 0, Width, Height);
+            GL.Viewport(0, 0, Width, Height); //canvasul unui browser
 
             double aspect_ratio = Width / (double)Height;
 
             Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+        
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref perspective);
+
+            Matrix4 lookat = Matrix4.LookAt(0, 0, 30, 0,0, 0, 0, 1, 0); //coordonatele camerei - primele 3
+            // urm 3 - pozitia targetului
+            // urm 3 - rotatia camerei
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            camera.SetCamera();
         }
 
-        /** Secțiunea pentru "game logic"/"business logic". Tot ce se execută în această secțiune va fi randat
-            automat pe ecran în pasul următor - control utilizator, actualizarea poziției obiectelor, etc. */
+  
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
-            base.OnUpdateFrame(e);
+            base.OnUpdateFrame(e); //bucla de control a opentk
+            // este partea logica - update-urile de logica
 
-            KeyboardState keyboard = OpenTK.Input.Keyboard.GetState();
-            MouseState mouse = OpenTK.Input.Mouse.GetState();
+            KeyboardState keyboard = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
 
-            // Se utilizeaza mecanismul de control input oferit de OpenTK (include perifcerice multiple, mai ales pentru gaming - gamepads, joysticks, etc.).
-            if (keyboard[OpenTK.Input.Key.Escape])
+            if (mouse[OpenTK.Input.MouseButton.Left] && mouse.X > 100)
+            {
+                camera.RotateRight();
+            }
+            else if (mouse[OpenTK.Input.MouseButton.Left] && mouse.X < 100)
+            {
+                camera.RotateLeft();
+            }
+
+
+            if (keyboard[Key.Escape])
             {
                 Exit();
-                return;
             }
-            else if (keyboard[OpenTK.Input.Key.P] && !keyboard.Equals(lastKeyPress))
+            else if(keyboard[Key.Up] && keyboard[Key.R] && red<1)
             {
-                // Ascundere comandată, prin apăsarea unei taste - cu verificare de remanență! Timpul de reacțieuman << calculator.
-                if (showCube == true)
-                {
-                    showCube = false;
-                }
-                else
-                {
-                    showCube = true;
-                }
+                red += 0.05;
             }
-            lastKeyPress = keyboard;
+            else if (keyboard[Key.Down] && keyboard[Key.R] && red>0)
+            {
+                red -= 0.05;
+            }
+            else if(keyboard[Key.Up] && keyboard[Key.B] && blue<1)
+            {
+                blue += 0.05;
+            }
+            else if(keyboard[Key.Down] && keyboard[Key.B] && blue>0)
+            {
+                blue -= 0.05;
+            }
+            else if (keyboard[Key.Up] && keyboard[Key.G] && green < 1)
+            {
+                green += 0.05;
+            }
+            else if (keyboard[Key.Down] && keyboard[Key.G] && green > 0)
+            {
+                green -= 0.05;
+            }
+            else if (keyboard[Key.Up] && keyboard[Key.A] && alpha < 1)
+            {
+                alpha += 0.05;
+                Console.WriteLine(alpha);
 
-            if (keyboard[OpenTK.Input.Key.Up])
-            {
-                translateAmount++;
             }
-            else if (keyboard[OpenTK.Input.Key.Down])
+            else if (keyboard[Key.Down] && keyboard[Key.A] && alpha > 0)
             {
-                // if (translateAmount > 0)
-                translateAmount--;
-            }
-
-            if(mouse[OpenTK.Input.MouseButton.Left])
-            {
-                rotateAmount = mouse.X;
+                alpha -= 0.05;
+                if(alpha < 0.05)
+                {
+                    alpha = 0;
+                }
+                Console.WriteLine(alpha);
             }
         }
 
-        /** Secțiunea pentru randarea scenei 3D. Controlată de modulul logic din metoda ONUPDATEFRAME().
-            Parametrul de intrare "e" conține informatii de timing pentru randare. */
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
 
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
 
-            Matrix4 lookat = Matrix4.LookAt(15, 50, 15, 0, 0, 0, 0, 1, 0);
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadMatrix(ref lookat);
+            DrawAxes();
 
-            GL.Translate(new Vector3(-translateAmount,0, -translateAmount));
-
-            GL.Rotate(rotateAmount,0,1,0);
-
-            // Exportăm controlul randării obiectelor către o metodă externă (modularizare).
-            if (showCube == true)
-            {
-                DrawCube();
-                DrawAxes_OLD();
-
-            }
+            DrawObjects();
 
             SwapBuffers();
-            Thread.Sleep(1);
         }
 
-        private void DrawAxes_OLD()
+        private void DrawAxes()
         {
             GL.Begin(PrimitiveType.Lines);
-
-            // X
+            // Desenează axa Ox (cu roșu).
             GL.Color3(Color.Red);
             GL.Vertex3(0, 0, 0);
-            GL.Vertex3(20, 0, 0);
-
-            // Y
             GL.Color3(Color.Blue);
-            GL.Vertex3(0, 0, 0);
-            GL.Vertex3(0, 20, 0);
-
-            // Z
+            GL.Vertex3(XYZ_SIZE, 0, 0);
+            // Desenează axa Oy (cu galben).
             GL.Color3(Color.Yellow);
             GL.Vertex3(0, 0, 0);
-            GL.Vertex3(0, 0, 20);
-
-
+            GL.Vertex3(0, XYZ_SIZE, 0);
+            // Desenează axa Oz (cu verde).
+            GL.Color3(Color.Green);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, XYZ_SIZE);
             GL.End();
         }
 
-        // Utilizăm modul imediat!!!
-        private void DrawCube()
+        private void DrawObjects()
         {
-            GL.Begin(PrimitiveType.Quads);
 
-            GL.Color3(Color.Silver);
-            GL.Vertex3(-1.0f, -1.0f, -1.0f);
-            GL.Vertex3(-1.0f, 1.0f, -1.0f);
-            GL.Vertex3(1.0f, 1.0f, -1.0f);
-            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Color4(red, green, blue, alpha);
+            GL.Vertex3(co[0][0], co[0][1], co[0][2]);
+            GL.Vertex3(co[1][0], co[1][1], co[1][2]);
+            GL.Vertex3(co[2][0], co[2][1], co[2][2]);
+            GL.End();
 
-            GL.Color3(Color.Honeydew);
-            GL.Vertex3(-1.0f, -1.0f, -1.0f);
-            GL.Vertex3(1.0f, -1.0f, -1.0f);
-            GL.Vertex3(1.0f, -1.0f, 1.0f);
-            GL.Vertex3(-1.0f, -1.0f, 1.0f);
-
-            GL.Color3(Color.Moccasin);
-
-            GL.Vertex3(-1.0f, -1.0f, -1.0f);
-            GL.Vertex3(-1.0f, -1.0f, 1.0f);
-            GL.Vertex3(-1.0f, 1.0f, 1.0f);
-            GL.Vertex3(-1.0f, 1.0f, -1.0f);
-
-            GL.Color3(Color.IndianRed);
-            GL.Vertex3(-1.0f, -1.0f, 1.0f);
-            GL.Vertex3(1.0f, -1.0f, 1.0f);
-            GL.Vertex3(1.0f, 1.0f, 1.0f);
-            GL.Vertex3(-1.0f, 1.0f, 1.0f);
-
-            GL.Color3(Color.PaleVioletRed);
-            GL.Vertex3(-1.0f, 1.0f, -1.0f);
-            GL.Vertex3(-1.0f, 1.0f, 1.0f);
-            GL.Vertex3(1.0f, 1.0f, 1.0f);
-            GL.Vertex3(1.0f, 1.0f, -1.0f);
-
-            GL.Color3(Color.ForestGreen);
-            GL.Vertex3(1.0f, -1.0f, -1.0f);
-            GL.Vertex3(1.0f, 1.0f, -1.0f);
-            GL.Vertex3(1.0f, 1.0f, 1.0f);
-            GL.Vertex3(1.0f, -1.0f, 1.0f);
-
+            GL.Begin(PrimitiveType.TriangleStrip);
+            GL.Color3(Color.Black);
+            GL.Vertex3(0, 0, 0);
+            GL.Color3(Color.White);
+            GL.Vertex3(0, -10, 0);
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(10, 0, 0);
             GL.End();
         }
 
         [STAThread]
         static void Main(string[] args)
         {
-
-            /**Utilizarea cuvântului-cheie "using" va permite dealocarea memoriei o dată ce obiectul nu mai este
-               în uz (vezi metoda "Dispose()").
-               Metoda "Run()" specifică cerința noastră de a avea 30 de evenimente de tip UpdateFrame per secundă
-               și un număr nelimitat de evenimente de tip randare 3D per secundă (maximul suportat de subsistemul
-               grafic). Asta nu înseamnă că vor primi garantat respectivele valori!!!
-               Ideal ar fi ca după fiecare UpdateFrame să avem si un RenderFrame astfel încât toate obiectele generate
-               în scena 3D să fie actualizate fără pierderi (desincronizări între logica aplicației și imaginea randată
-               în final pe ecran). */
             using (Program example = new Program())
             {
-
-                // Verificați semnătura funcției în documentația inline oferită de IntelliSense!
                 example.Run(30.0, 0.0);
             }
         }
     }
 }
+
